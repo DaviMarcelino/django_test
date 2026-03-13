a6b9471c139a53ec67c25eff85938ab219303224
Davi Marcelino da Silva <marcelinodavi265@gmail.com>
2026-03-13 19:10:34 -0300

    templates

--- projeto/urls.py
+++ projeto/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index_view),
+    path('teste/',views.teste_view),
     path('tarefas/', include("tarefas.urls"))
 ]
--- tarefas/urls.py
+++ tarefas/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-path('', views.tarefas_home)
-path("adicionar/", views.tarefa_adicionar)
+path('', views.tarefas_home),
+path("adicionar/", views.tarefas_adicionar)
 ]
